9cb28d9956997c3992df3f1118e14fc0a459a420
StevenLomon <steven.lennartsson@gmail.com>
2024-08-05 11:43:47 +0200

    save_for_class method implemented

--- camera-train-model.py
+++ camera-train-model.py
@@ -10,8 +10,8 @@ class App:
         self.window = window
         self.window_title = window_title
 
-        # An array to keep track of the name of the class followed by a number
-        # self.counters = [1, 1]
+        # An int to keep track of the name of the file name
+        self.counters = 1
 
         # self.model = 
 
@@ -48,3 +48,21 @@ class App:
 
     def auto_predict_toggle(self):
         self.auto_predict = not self.auto_predict # Negating what it already is, chaning its current state
+
+    def save_for_class(self):
+        ret, frame = self.camera.get_frame()
+        if not os.path.exists('ducks'):
+            os.mkdir('ducks')
+
+        # We're gonna choose to write with color rather than grayscale since the yellow (default and the 
+        # color of my duck) rather distinguishable feature of a rubber duck :))
+        filename = f"ducks/frame{self.counter}.jpg"
+        cv.imwrite(filename, cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+
+        # Resize for processing purposes
+        img = PIL.Image.open(filename)
+        img.thumbnail(150,150), PIL.Image.ANTIALIAS
+        img.save(filename)
+
+        # Increase the counter to continue keeping track of file names
+        self.counter += 1
