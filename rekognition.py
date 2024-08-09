fdb8276f3bb059a52a823bd3ef29746c6dfca609
StevenLomon <steven.lennartsson@gmail.com>
2024-08-09 07:56:26 +0200

    We can see Rekognition labels!

--- rekognition.py
+++ rekognition.py
@@ -1,7 +1,25 @@
 import boto3
+from rich import print
 
 # Create an S3 object
 s3 = boto3.client('s3')
 
 # # Test to upload an image PASSED
-# s3.upload_file('rubber-duck.jpeg', 'cs50-final-project-rubber-duck-rekognition-bucket', 'rubber-duck.jpeg')
+# s3.upload_file('rubber-duck.jpeg', 'cs50-final-project-rubber-duck-bucket', 'rubber-duck.jpeg')
+
+# Create an AWS Rekognition object
+rekognition = boto3.client('rekognition', region_name='eu-central-1')
+
+response = rekognition.detect_labels(
+    Image = {
+        'S3Object': {
+            'Bucket': 'cs50-final-project-rubber-duck-bucket',
+            'Name': 'rubber-duck.jpeg'
+        },
+    },
+    MaxLabels = 10,
+    MinConfidence = 80
+)
+
+labels = response['Labels']
+print(labels)
