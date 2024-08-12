beee7d54936d6baa05362cadfeee1ad5377b1001
StevenLomon <steven.lennartsson@gmail.com>
2024-08-12 15:11:00 +0200

    Updated the database schema for bounding box data

--- init_db.py
+++ init_db.py
@@ -9,9 +9,12 @@ def initialize_database():
         duck_results (
             id TEXT PRIMARY KEY,
             duck_found BOOLEAN NOT NULL,
+            bounding_box_available BOOLEAN NOT NULL,
             confidence_score FLOAT,
+            bounding_box_data TEXT,
             s3_key TEXT NOT NULL,
             s3_url TEXT NOT NULL,
+            s3_url_bounding_box TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     """
